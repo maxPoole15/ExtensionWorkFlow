9b1b2024134391ec06d5208a8eb6df82b3e5b956
maxPoole15 <64996563+maxPoole15@users.noreply.github.com>
2023-10-11 19:51:00 -0600

    11

--- background.js
+++ background.js
@@ -14,19 +14,6 @@ chrome.action.onClicked.addListener(async (tab) => {
             text: nextState,
       });
     }
-    if (nextState === "ON") {
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-          files: ["focus-mode.css"],
-          target: { tabId: tab.id },
-        });
-      } else if (nextState === "OFF") {
-        // Remove the CSS file when the user turns the extension off
-        await chrome.scripting.removeCSS({
-          files: ["focus-mode.css"],
-          target: { tabId: tab.id },
-        });
-    }
 });
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -34,6 +21,20 @@ chrome.runtime.onInstalled.addListener(() => {
         text: "OFF",
     });
 });
+
+if (nextState === "ON") {
+    // Insert the CSS file when the user turns the extension on
+    await chrome.scripting.insertCSS({
+      files: ["focus-mode.css"],
+      target: { tabId: tab.id },
+    });
+  } else if (nextState === "OFF") {
+    // Remove the CSS file when the user turns the extension off
+    await chrome.scripting.removeCSS({
+      files: ["focus-mode.css"],
+      target: { tabId: tab.id },
+    });
+}
 
 
 
